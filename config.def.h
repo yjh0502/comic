4b0220c2a8853f0e7996c6a1cb83a468537b768f
Jihyun Yu <yjh0502@gmail.com>
2014-05-24 00:06:38 +0900

    update config.def.h

--- config.def.h
+++ config.def.h
@@ -1,16 +1,20 @@
 
-#define MODKEY Mod1Mask
 
+#define ARCHIVE_BLOCK_SIZE  10240
+#define IMAGE_SIZE_LIMIT    1000 * 1000 * 10
+#define TITLE_LENGTH_LIMIT  1024
+
+#define MODKEY Mod1Mask
 static Key keys[] = {
     /* modifier       key        function        argument */
     { 0,              XK_q,      quit,          { 0 } },
-    { 0,              XK_p,      prev,          {.i = 1 } },
-    { 0,              XK_n,      next,          {.i = 1 } },
-    { 0,              XK_b,      prev,          {.i = 10 } },
-    { 0,              XK_f,      next,          {.i = 10 } },
+    { 0,              XK_p,      seek,          {.i = -1 } },
+    { 0,              XK_n,      seek,          {.i = 1 } },
+    { 0,              XK_b,      seek,          {.i = -10 } },
+    { 0,              XK_f,      seek,          {.i = 10 } },
 };
 
 static Button buttons[] = {
     /*event mask      button          function        argument */
-    { 0,              Button1,        next,           {0} },
+    { 0,              Button1,        seek,           {.i = 1 } },
 };
