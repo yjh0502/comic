7ca65f94731374c341d225460f06c87161060eee
Jihyun Yu <yjh0502@gmail.com>
2014-05-27 11:30:43 +0900

    Versioned sort at comic_dir.sh

--- comic_dir.sh
+++ comic_dir.sh
@@ -4,5 +4,5 @@ DIR="$1";
 [ -z "$DIR" ] && DIR="./";
 
 cd "${DIR}" || exit 1;
-find -name "*.jpg" -or -name "*.JPG" | sort \
+find -name "*.jpg" -or -name "*.JPG" | sort -V \
     | xargs -d "\n" comic;
--- config.mk
+++ config.mk
@@ -12,7 +12,7 @@ X11LIB = /usr/X11R6/lib
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -ljpeg -larchive # -lfreeimage
+LIBS = -lc -L${X11LIB} -lX11 -ljpeg -larchive
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE
